87823ffbf47d2820faa7624bdaee2fb4ae36bfdd
Nicolas Vasilache <nicolasvasilache@users.noreply.github.com>
2025-12-11 18:12:11 +0100

    [NFC] Add a benchmark script
    Signed-off-by: Nicolas Vasilache <nicolasvasilache@users.noreply.github.com>

---
+++ mlir_kernels/benchmarks/run_benchmarks.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -ex
+
+python  ./mlir_kernels/benchmarks/benchmark_copy_1d.py
+python ./mlir_kernels/benchmarks/benchmark_mfma_dword4_mxnxk_16x16x16_f16f16f32.py
+python ./mlir_kernels/benchmarks/benchmark_gemm_dword4_mxnxk_16x16x16_f16f16f32.py
+python ./mlir_kernels/benchmarks/benchmark_gemm_dword4_mxnxk_16x16x16_f16f16f32.py --mlir-filename gemm_sched_dword4_mxnxk_16x16x16_f16f16f32.mlir
