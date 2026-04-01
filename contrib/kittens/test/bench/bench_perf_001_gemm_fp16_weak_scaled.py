d2f3bdbd431a19963a97276351b30c0b0378fb64
Nicolas Vasilache <nicolasvasilache@users.noreply.github.com>
2026-04-01 09:40:10 +0200

    NFC - fix kittens import
    Signed-off-by: Nicolas Vasilache <nicolasvasilache@users.noreply.github.com>

--- contrib/kittens/test/bench/bench_perf_001_gemm_fp16_weak_scaled.py
+++ contrib/kittens/test/bench/bench_perf_001_gemm_fp16_weak_scaled.py
@@ -15,6 +15,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "..", ".."))
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "..", "..", ".."))
 
 from kittens.gemm_config import WeakScaledMappedGemmInstance
